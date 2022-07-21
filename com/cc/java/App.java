663203978eb7af151101913a146cccf2d614e1e7
Robert <robelix@gmx.de>
2022-07-21 07:59:19 +0200

    Meein Prog in der Schule

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,10 +3,23 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
-    }
-
     
+        Konto konto1 = new Konto(10000);
+            System.out.println(konto1.getKontostand());
+                kontostand.setKontostand(setKontostand()*2);
+        
+        Konto konto2 = new Konto(10000);
+            System.out.println(konto2.getKontostand());
+                kontostand.setKontostand(setKontostand()*3);
 
+        Konto konto3 = new Konto(10000);
+            System.out.println(konto3.getKontostand());
+               kontostand.setKontostand(setKontostand()*10);   
+        
+        System.out.println("-----------------------------");
+        System.out.println(konto1.getKontostand());
+        System.out.println(konto2.getKontostand());
+        System.out.println(konto3.getKontostand());
+    }
 }
 
